88eb14edc5f9286e3bed0420d506237f11c3c735
shunsuke.noguchi <kesunshuchiguno2525@gmail.com>
2025-06-01 21:54:59 +0900

    Initial Commit

---
+++ LeangainsFirst/LeangainsFirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeangainsFirstApp.swift
+//  LeangainsFirst
+//
+//  Created by 野口隼輔 on 2025/06/01.
+//
+
+import SwiftUI
+
+@main
+struct LeangainsFirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LeangainsFirstTests/LeangainsFirstTests.swift
@@ -0,0 +1,17 @@
+//
+//  LeangainsFirstTests.swift
+//  LeangainsFirstTests
+//
+//  Created by 野口隼輔 on 2025/06/01.
+//
+
+import Testing
+@testable import LeangainsFirst
+
+struct LeangainsFirstTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
